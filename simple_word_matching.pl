8a89df3ac30d0033f4b671b2586623006fc867c1
Luis José Sánchez <luisjoseprofe@gmail.com>
2024-11-13 10:20:56 +0100

    Ejemplos actualizados

--- simple_word_matching.pl
+++ simple_word_matching.pl
@@ -8,3 +8,21 @@ else {
     print "It doesn't match\n";
 }
 
+$_ = "Antes de nada, una scaleup ralentiza el cambio climático. Nos manifestamos contra la destrucción de los bosques tropicales. El CTO está en el recreo, tomando café y un pitufo de tortilla. El branding cierra una ronda inicial de 10M €.";
+
+
+if (/micro/) {
+    print "It matches\n";
+}
+else {
+    print "It doesn't match\n";
+}
+
+if (/recreo/) {
+    print "It matches\n";
+}
+else {
+    print "It doesn't match\n";
+}
+
+
